eb54f515f12e68b61df1640e3f1675efdce089d6
Benjamin Jones <benjaminfjones@gmail.com>
2012-07-17 23:23:45 -0500

    added boilerplate Main.hs

---
+++ Main.hs
@@ -0,0 +1,13 @@
+--
+-- Main.hs
+-- Copyright 2012 Benjamin F Jones <benjaminfjones@gmail.com>
+--
+-- A Labyrinth game inspired by the Monad.Reader special issue. See README.md.
+--
+{-# OPTIONS_GHC -Wall -Werror #-}
+module Main where
+
+import Labyrinth
+
+main :: IO ()
+main = putStrLn "Coming soon..."
