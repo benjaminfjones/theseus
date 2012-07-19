2a58f837b71782566fff639e917300bf22058755
Benjamin Jones <benjaminfjones@gmail.com>
2012-07-19 12:22:06 -0500

    added SimpleLabyrinth type

--- LabData.hs
+++ LabData.hs
@@ -8,14 +8,22 @@ module LabData where
 
 import Labyrinth
 
+-- | SimpleLabyrinth is a Zipper that contains a cooredinate pair (Int, Int) at each
+-- node.
+type SimpleLabyrinth = Zipper (Int, Int)
+
 -- | An example labyrinth
-l0 :: Node (Int, Int)
-l0 = Fork (0,2) 
-     (Fork (-2, 0) 
-           (DeadEnd (0,-2)) 
-           (DeadEnd (-1,0)))
-     (Passage (2,0) 
-              (Fork (1,0)
-                    (Passage (0,1)
-                             (DeadEnd (0,0)))
-                    (DeadEnd (0,-1))))
+l0 :: SimpleLabyrinth
+l0 = ( []
+     , Fork (0,2) 
+       (Fork (-2, 0) 
+             (DeadEnd (0,-2)) 
+             (DeadEnd (-1,0)))
+       (Passage (2,0) 
+                (Fork (1,0)
+                      (Passage (0,1)
+                               (DeadEnd (0,0)))
+                      (DeadEnd (0,-1))))
+     )
+
+
