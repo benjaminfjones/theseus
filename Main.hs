a28f47ee7f2319606980a56b2cdc50172b2fed27
Benjamin Jones <benjaminfjones@gmail.com>
2012-07-19 12:23:27 -0500

    moved example labyrinth to LabData.hs

--- Main.hs
+++ Main.hs
@@ -7,22 +7,11 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 module Main where
 
-import Labyrinth
+import Labyrinth ()
+import LabData (l0)
 
 -- | main, print a boilerplate
 main :: IO ()
 main = do
     putStrLn "Coming soon..."
     print l0
-
--- Example labyrinth
-l0 :: Node (Int, Int)
-l0 = Fork (0,2) 
-     (Fork (-2, 0) 
-           (DeadEnd (0,-2)) 
-           (DeadEnd (-1,0)))
-     (Passage (2,0) 
-              (Fork (1,0)
-                    (Passage (0,1)
-                             (DeadEnd (0,0)))
-                    (DeadEnd (0,-1))))
